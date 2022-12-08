f1404f268e3e70937ec1d97699e66f47777dcc9a
Kallolghosh1 <120078846+Kallolghosh1@users.noreply.github.com>
2022-12-08 14:11:08 +0530

    Add files via upload

--- sum.java
+++ sum.java
@@ -4,31 +4,46 @@ import java.util.Scanner;
 public class sum {
 
 
-	 	public static void main(String[] args) {
-	 		int num, number, temp, total = 0;
-	        Scanner scanner = new Scanner(System.in);
-	        
-	        System.out.println("Enter a 3 digit number: ");
-	        num = scanner.nextInt();
-	        
-	        scanner.close();
-	        number = num;
-	        
-	        for (; number != 0; number /= 10)
-	        {
-	            temp = number % 10;
-	            total = total + temp*temp*temp;
-	        }
-
-	        if(total == num)
-	            System.out.println(num + " is an Armstrong number");
-	        else
-	            System.out.println(num + " is not an Armstrong number");
-
+	public static boolean isStrongNumber(int num) {
+	 		int fact = 1;
+			int sum = 0;
+			int temp = num;
+			
+			while(temp > 0) {
+				// Get the last digit of the number
+				int lastDigit = temp % 10;
+				
+				// Calculating the factorial of last digit
+				for(int i=1; i<=lastDigit; i++) {
+					fact *= i;
+				}
+				
+				// Adding the factorial of last digit to sum
+				sum += fact;
+				
+				// Resetting the factorial
+				fact = 1;
+				
+				// Removing the last digit of the number
+				temp = temp/10;
+			}
+			
+			// Check if sum is equal to the given number
+			if(sum == num)
+				return true;
+			
+			return false;
+		}
+		
+		public static void main(String[] args) {
+			int num = 145;
+			
+			if(isStrongNumber(num))
+				System.out.println(num + " is a strong number");
+			else
+				System.out.println(num + " is not a strong number");
+		}
 	}
-
-}
-	 
 
 
 
