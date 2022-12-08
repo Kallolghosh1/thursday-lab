d3d4e1f2b9f9a31d87f4a2361e15a5a6cdde4b17
Kallolghosh1 <120078846+Kallolghosh1@users.noreply.github.com>
2022-12-08 14:21:14 +0530

    Add files via upload

---
+++ ARMSTRONGNO.java
@@ -0,0 +1,29 @@
+package kallol1;
+import java.util.Scanner;
+public class ARMSTRONGNO {
+
+	public static void main(String[] args) {
+	
+			 int num, number, temp, total = 0;
+		        Scanner scanner = new Scanner(System.in);
+		        
+		        System.out.println("Enter a 3 digit number: ");
+		        num = scanner.nextInt();
+		        
+		        scanner.close();
+		        number = num;
+		        
+		        for (; number != 0; number /= 10)
+		        {
+		            temp = number % 10;
+		            total = total + temp*temp*temp;
+		        }
+
+		        if(total == num)
+		            System.out.println(num + " is an Armstrong number");
+		        else
+		            System.out.println(num + " is not an Armstrong number");
+
+		}
+
+	}
